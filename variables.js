599bb99509fa4d9396e512f808df100db98ed9c7
Lithembar <mlenzelithemba@gmail.com>
2024-11-11 11:48:01 +0200

    new notes added

---
+++ variables.js
@@ -0,0 +1,86 @@
+// variables
+// strings
+
+// let firstname= "Lithemba"
+
+// const firstname = "Lithemba"
+
+// var firstname = "Lithemba"
+
+// console.log(firstname);
+
+// Numbers
+// let number1 = 10.5
+
+// booleans
+// let value = true/ false 
+
+// undefined
+// let name
+
+// null
+// let value = null
+// console.log(firstname )
+
+// let firstname = "Lithemba"
+// firstname = 3
+// console.log(`my name is ${firstname}`)
+// console.log("my name is "+ firstname + num)
+// let myArray = ['anything',2, true, 'string']
+// console.log(myArray[0])
+
+// camelcase --> myArray (always used in 15)
+
+// an object literal -declare (make) an obect
+//let table = {
+  //  material:"wood",
+  //  no_of_legs:4,
+  //  occupied:true,
+//}
+// bracket notation
+//console.log(table['material']);
+
+// dot notation
+//console.log(table.material);
+
+// destructuring
+//let {material} = table//wood
+//let {occupied} = table//wood
+//console.log(material);
+
+//functions
+//() -> parentheses
+//[] -> brackets
+//{} -> braces
+ //function greeting(a,b){
+  //  console.log('This is a greeting'+ (a+b));
+  //console.log("H1 $(fullname)")
+//}
+// 384 are arguments
+//greeting(3,4)
+
+//let num1 =5
+//++numb1 pre increment
+//numb1 post increment
+//--numb1 pre decrement
+//numb1-- post decrement
+//console.log(numb1--);
+//console.log(numb1);
+//11 NOV
+
+//converting a number to a String
+//let number =number.toString
+
+//String Method
+//let myString = "something"
+//show us the lenght on the string
+//let lenght = myString.length;
+
+// make text capital --> mystring.toupperCase()
+// make text small letter--> mystring.toLowerCase()
+//let split =myString.split('')
+//console.log(split)
+
+//let split =myString.split('')
+//console.log(split)
+
