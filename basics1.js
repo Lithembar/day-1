58952e50e7cdb7d420141ef1b71e04b693e9724b
Lithembar <mlenzelithemba@gmail.com>
2024-11-15 13:54:35 +0200

    exercise1w4.js

---
+++ basics1.js
@@ -0,0 +1,19 @@
+
+let firstname="Lithemba"
+console.log(firstname)
+
+let lastname="Mlenze"
+console.log(lastname)
+
+let age
+
+let fullname=firstname+lastname
+console.log(fullname)
+
+function greeting(a,b){
+    console.log('This is a greeting'+ ( firstname+lastname));
+}
+greeting(firstname+lastname)
+
+age=20
+console.log(age)
--- variables.js
+++ variables.js
@@ -84,21 +84,21 @@
 //let split =myString.split('')
 //console.log(split)
 
-let value = 2
-switch (value){
-    case 1:
-        console.log("It is sunny");
-        break;
-    case 2:
-        console.log("It is cloudy");
-        break;
-    case 3:
-        console.log("It is raining");
-        break;
-    default:
-        console.log("Couldn't read the value");
-        break;
-}
+//let value = 2
+//switch (value){
+  //  case 1:
+    //    console.log("It is sunny");
+      //  break;
+    //case 2:
+      //  console.log("It is cloudy");
+        //break;
+    //case 3:
+      ///  console.log("It is raining");
+        //break;
+    //default:
+      //  console.log("Couldn't read the value");
+        //break;
+//}
 
 
    //version 2 of switch statements
@@ -173,11 +173,11 @@ switch (value){
 
 //with return
 
-function returned(a){
-     return a
-}
+//function returned(a){
+  //   return a
+//}
 
-console.log('The number is' + returned('forty five'));
+//console.log('The number is' + returned('forty five'));
 
 //funtion expression
 //let run = function(){
@@ -194,10 +194,10 @@ console.log('The number is' + returned('forty five'));
 
 //Self-involving funtion peak anonymous fuction
 
-(function(){
-  console.log("haha I'm a ghost");
+//(function(){
+  //console.log("haha I'm a ghost");
   
-})()
+//})()
 
 //arrow funtion
 //let walk = ()=>{
@@ -219,4 +219,93 @@ console.log('The number is' + returned('forty five'));
 //console.log('You are walking');
 
 //SCOPING LOCAL AND GLOBAL
+
+
+
+
+//15 NOV
+
+//Objects
+// let person={
+//     name:"Yandile",
+//     surname:"Matyityilili",
+//     hairType:"bold"
+// }
+// console.log(person.name); //Access using Dot notation
+//Or
+// console.log(person['name']); //Acces using bracket notation
+//Or
+// let {name}= person
+// console.log(name)//Accessing values in objects usingDestruction
+// let person={
+//     name:"Yandile",
+//     surname:"Matyityilili",
+//     hairType:"bold",
+//     get fullname(){
+//         return this.name + this.surname
+//     }
+// }
+// person.favColour = 'Purple'
+// console.log(person.fullname);
+// let person2={
+//     name:"Surur",
+//     surname:"Samaai",
+//     hairType:"Curly",
+//     favSport:"Formmula",
+//     get fullname(){
+//         return this.name + this.surname
+//     }
+// }
+// console.log(person2.fullname);
+// Factory Functions
+// function createPerson(name,surname,hairType){
+//     return{
+//         name:name,
+//         surname:surname,
+//         hairType:hairType
+//     }
+// }
+// let person = createPerson('Yandile','Matyityilili','Bold')
+// console.log(person);
+// person.name ="New name"
+// function CreatePerson(name,surname,hairType){
+//     this.name = name;
+//     this.surname = surname;
+//     this.hairType = hairType;
+// }
+//  let person = new CreatePerson('Yandile','Matyityilili','Bold')
+// CreatePerson.prototype.fullname =function(){
+//     console.log('Hi' + this.name);
+//  }
+//  let  input = prompt('What is your name?')
+//  console.log(input);
+function CreatePerson(name,surname,hairType){
+    this.name = name;
+    this.surname = surname;
+    this.hairType = hairType;
+}
+ let person = new CreatePerson('Yandile','Matyityilili','Bold')
+ let person2 = new CreatePerson('Ethan','Brown','Straight')
+ let people = [person,person2]
+ console.log(people);
+for (let object of people){
+    if (object.hairType == 'Straight'){
+        console.log(object.name)
+    }
+}
+let cars =[
+    {
+        brand:'Toyota',
+        models:[{type:'Corolla'}]
+    },
+    {
+        brand:'BMW',
+        models:[{}],
+    }
+]
+
+
+
+
+
 
